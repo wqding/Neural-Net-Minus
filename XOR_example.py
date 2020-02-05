74baacca37defc30ba11dac1c890e54be67b9e44
Adam Ding <wqding@uwaterloo.ca>
2020-02-04 23:31:41 -0500

    changed NN parameters to prevent overfitting

--- XOR_example.py
+++ XOR_example.py
@@ -6,14 +6,14 @@ import numpy as np
 
 # create a neural network with 4 layers, 2 nodes in each layer except the last layer
 # learning rate = 0.2
-neural_network = NNM.neural_network([2, 2, 2, 1], 0.2)
+neural_network = NNM.neural_network([2, 3, 1], 0.2)
 
 # creating paired dataset
 input_arr = [[1, 1], [0, 0], [1, 0], [0, 1]]
 target_arr = [[0], [0], [1], [1]]
 
 # training
-for i in range(50000):
+for i in range(100000):
     idx = np.random.randint(0,4)
     neural_network.train(input_arr[idx], target_arr[idx])
     
